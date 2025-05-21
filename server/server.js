60bd6512a6629e4f50edcbfecb33674df2699099
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-05-21 18:36:47 +0530

    use env-based CORS origin setup

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 // const PORT = process.env.PORT || 3000;  // Use environment variable for port
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 const router = require('./router/authRouter');
 const tempRouter = require('./router/templateRouter');
 const cors = require('cors');
