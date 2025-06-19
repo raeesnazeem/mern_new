60f439bded01981dc47031f67fc901d24dde12c6
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-06-19 20:52:45 +0530

    custom chatbot file added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx'; 
 import './App.css'; 
-import './styles/CustomChatbot.css'; 
+import './styles/Customchatbot.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
